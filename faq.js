46f6a307b77e5e929970746035711d6f8eeeff98
Orunto Eniola <98480650+orunto@users.noreply.github.com>
2022-03-15 19:05:58 +0100

    Add files via upload

--- faq.js
+++ faq.js
@@ -14,7 +14,11 @@ window.onload=function (){
     var down = document.getElementById("down");
     var black = document.getElementById("black");
     var plus = document.getElementById("plus");
-    var answer = document.getElementById("answer");
+    var answer1 = document.getElementById("answer1");
+    var answer2 = document.getElementById("answer2");
+    var answer3 = document.getElementById("answer3");
+    var answer4 = document.getElementById("answer4");
+    var answer5 = document.getElementById("answer5");
     one.addEventListener("click", twounder);
     two.addEventListener("click", threeunder);
     three.addEventListener("click", fourunder);
@@ -30,7 +34,7 @@ window.onload=function (){
         four.style.top = "402px";
         five.style.top = "444px";
         plus.style.transform = "rotate(45deg)";
-        answer.style.opacity = "1";
+        answer1.style.opacity = "1";
         one.removeEventListener("click", twounder);
         one.addEventListener("click", retract);
     }
@@ -42,7 +46,11 @@ window.onload=function (){
         five.style.top = "372px";
         down.style.top = "40px";
         plus.style.transform = "rotate(90deg)";
-        answer.style.opacity = "0";
+        answer1.style.opacity = "0";
+        answer2.style.opacity = "0";
+        answer3.style.opacity = "0";
+        answer4.style.opacity = "0";
+        answer5.style.opacity = "0";
         one.removeEventListener("click", retract);
         two.removeEventListener("click", retract);
         three.removeEventListener("click", retract);
@@ -59,6 +67,7 @@ window.onload=function (){
         three.style.top = "358px";
         four.style.top = "402px";
         five.style.top = "444px";
+        answer2.style.opacity = "1";
         two.removeEventListener("click", threeunder);
         two.addEventListener("click", retract);
     }
@@ -66,18 +75,21 @@ window.onload=function (){
     function fourunder () {
         four.style.top = "402px";
         five.style.top = "444px";
+        answer3.style.opacity = "1";
         three.removeEventListener("click", fourunder);
         three.addEventListener("click", retract);
     }
 
     function fiveunder () {
         five.style.top = "444px";
+        answer4.style.opacity = "1";
         four.removeEventListener("click", fiveunder);
         four.addEventListener("click", retract);
     }
 
     function downunder () {
         down.style.top = "140px";
+        answer5.style.opacity = "1";
         five.removeEventListener("click", downunder);
         five.addEventListener("click", retract);
     }
