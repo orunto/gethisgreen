645c36a503722871c8c36209b62b09ff69cf534c
Orunto Eniola <98480650+orunto@users.noreply.github.com>
2022-03-24 19:38:21 +0100

    Add files via upload

--- products/product.js
+++ products/product.js
@@ -3,9 +3,19 @@ window.onload=function (){
     var threespan = document.getElementById("threespan");
     var sel = document.getElementById("counter");
     var sele = document.getElementById("counter2");
+    var inkbutton = document.getElementById("inkbutton");
+    var inkpicker = document.getElementById("inkpicker");
+    var redink = document.getElementById("redink");
+    var greenink = document.getElementById("greenink");
+    var blackcolor = document.getElementById("blackcolor");
+    var bluecolor = document.getElementById("bluecolor");
+    var greencolor = document.getElementById("greencolor");
+    var redcolor = document.getElementById("redcolor");
 
     onespan.addEventListener("click", thick);
     threespan.addEventListener("click", thick2);
+    inkbutton.addEventListener("click", appear);
+    inkpicker.addEventListener("click", conjure);
 
     function thick () {
         onespan.style.border = "2px solid black";
@@ -26,6 +36,38 @@ window.onload=function (){
         onespan.style.border = "1px solid #676767";
         onespan.addEventListener("click", thick);
         threespan.addEventListener("click", thick2);
+    }
+
+    function appear () {
+        greenink.style.opacity= "1";
+        redink.style.opacity= "1";
+        inkbutton.removeEventListener("click", appear);
+        inkbutton.addEventListener("click", disappear);
+    }
+
+    function disappear () {
+        greenink.style.opacity= "0";
+        redink.style.opacity= "0";
+        inkbutton.removeEventListener("click", disappear);
+        inkbutton.addEventListener("click", appear);
+    }
+
+    function conjure () {
+        greencolor.style.opacity= "1";
+        redcolor.style.opacity= "1";
+        blackcolor.style.opacity= "1";
+        bluecolor.style.opacity= "1";
+        inkpicker.removeEventListener("click", conjure);
+        inkpicker.addEventListener("click", disconjure);
+    }
+
+    function disconjure () {
+        greencolor.style.opacity= "0";
+        redcolor.style.opacity= "0";
+        blackcolor.style.opacity= "0";
+        bluecolor.style.opacity= "0";
+        inkpicker.removeEventListener("click", disconjure);
+        inkpicker.addEventListener("click", conjure);
     }
 
     for(var i = 1;i<101;i++){
